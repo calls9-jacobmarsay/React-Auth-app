9cccb886a5997cb9ab0e369e53d00519161ed280
Jacob Marsay <jacobmarsay@calls9.com>
2023-01-05 17:11:17 +0000

    Conditionally making routes available

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import { Switch, Route } from 'react-router-dom';
-
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
+  const loggedIn = useSelector((state) => state.auth.isLoggedIn);
+
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
+        {loggedIn && (
+          <>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+            <Route path="/profile">
+              <UserProfile />
+            </Route>
+          </>
+        )}
+        {!loggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
+        <Route path="*">
+          <Redirect to="/auth" />
         </Route>
       </Switch>
     </Layout>
