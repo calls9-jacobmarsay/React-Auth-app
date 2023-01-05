056b51ccf9f1495ec0db50369d7beea0940a2670
Jacob Marsay <jacobmarsay@calls9.com>
2023-01-05 14:14:14 +0000

    conditional rendering added for login and logout

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,24 +1,40 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { authActions } from "../../store/store";
+import { useSelector, useDispatch } from "react-redux";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+  const dispatch = useDispatch();
+  const login = useSelector((state) => state.auth.isLoggedIn);
+  console.log(login);
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
-      <Link to='/'>
+      <Link to="/">
         <div className={classes.logo}>React Auth</div>
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!login && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+          {login && (
+            <>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <button onClick={logoutHandler}>Logout</button>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
